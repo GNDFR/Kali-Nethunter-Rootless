feae99d7ad5c7dc4d08e3142e4fee90d5d329659
GNDFR <kimhyowoo435@gmail.com>
2025-06-24 08:10:32 +0900

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,4 @@
+pkg update && pkg install wget -y
+wget -O install-nethunter-termux https://gitlab.com/kalilinux/nethunter/build-scripts/kali-nethunter-rootless/raw/main/install-nethunter-termux
+chmod +x install-nethunter-termux
+./install-nethunter-termux
