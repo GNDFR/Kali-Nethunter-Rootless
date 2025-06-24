c72fea1a084b7cec24581400d7e5856b09a41f99
GNDFR <kimhyowoo435@gmail.com>
2025-06-24 16:58:50 +0900

    Update and rename Install.sh to SimpleInstall.sh

--- Install.sh
+++
@@ -1,4 +0,0 @@
-pkg update && pkg install wget -y
-wget -O install-nethunter-termux https://gitlab.com/kalilinux/nethunter/build-scripts/kali-nethunter-rootless/raw/main/install-nethunter-termux
-chmod +x install-nethunter-termux
-./install-nethunter-termux
